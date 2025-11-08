32ad549800df178b5e946cb229e75d1b076d2099
taylor <fematr@oregonstate.edu>
2025-11-08 09:01:34 -0800

    Add controller/route for users

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,15 @@
-import pg from "pg";
-import "./dotenv.js";
+import pg from 'pg';
+import 'dotenv/config';
 
 const config = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
-  port: process.env.PGPORT,
+  port: Number(process.env.PGPORT),
   database: process.env.PGDATABASE,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, 
   },
 };
 
-const pool = new pg.Pool(config);
-export default pool;
+export const pool = new pg.Pool(config);
