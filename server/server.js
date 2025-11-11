de1beb18ab4bc3973d23e5a0d26ae3d214bc4bb9
taylor <fematr@oregonstate.edu>
2025-11-10 16:23:48 -0800

    Back and front end connected for Memories table ONLY

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import usersRouter from "./routes/users.js";
 import tagsRouter from "./routes/tags.js";
 import memoriesRouter from "./routes/memories.js";
-  
+
 const app = express();
 
 app.use(express.json());
@@ -12,9 +12,11 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Hello</h1>");
 });
-app.use("/users", usersRouter);
-app.use("/tags", tagsRouter);
-app.use("/memories", memoriesRouter);
+
+// change routes to match frontend
+app.use("/api/users", usersRouter);
+app.use("/api/tags", tagsRouter);
+app.use("/api/memory", memoriesRouter);
 
 const PORT = 3001;
 
